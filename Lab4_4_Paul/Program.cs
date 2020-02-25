fd1aacb39e75dc1f75b6c64d6bfd3583713a1524
HipolitoRosado <paulserrano58@gmail.com>
2020-02-25 12:28:55 -0600

    Add project files.

---
+++ Lab4_4_Paul/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab4_4_Paul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] blackListWord = {"viagra", "buy", "XXX", "free money",
+            "lifetime offer", "send money", "bank account", "nigeria", "onlinepharmacy", "h8te", "meet girls" };
+            
+
+            string message = Console.ReadLine();
+            message = message.ToLower();
+            Console.WriteLine(message);
+            bool isSpam = false;
+            for (int i = 0; i < blackListWord.Length; i++)
+            {
+                if (message.Contains(blackListWord[i]))
+                {
+                    isSpam = true;
+
+                }
+            }
+               if(isSpam == true)
+            {
+                Console.WriteLine("The message contains a spam");
+            }
+                
+                    else
+                {
+                    Console.WriteLine("The message is not spam.");
+                }
+
+            }
+        }
+        
+    }
+
+   
